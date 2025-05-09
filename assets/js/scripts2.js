ab230444ba40b68930437ca138664948d3029d5b
D7460N <dragon@dragontheory.com>
2025-05-09 19:30:03 -0400

    Added project meta data

--- assets/js/scripts2.js
+++ assets/js/scripts2.js
@@ -17,6 +17,11 @@ const API_MAP = {
 // Root URL for the mock API
 const API_ROOT = "https://67d944ca00348dd3e2aa65f4.mockapi.io"
 const AUTHOR = "D7460N" // Default author for new and updated records
+const VERSION = "0.0.1" // Version of the application
+const DATE = new Date().toISOString() // Current date in ISO format
+const APP_NAME = "D7460N DHCP Web UI" // Application name for the title
+const APP_DESC = "A web interface for managing DHCP entries" // Application description
+const APP_COPYRIGHT = `© ${new Date().getFullYear()} ${AUTHOR}. All rights reserved.` // Copyright notice
 
 // DOM elements and logic wiring
 const ul = document.querySelector("main article ul") // UL used to list entries on the "option-types" tab
