4da4882da1c68c50c26dfd9498e80265cef3207c
D7460N <80736+dragontheory@users.noreply.github.com>
2025-07-07 17:57:59 -0400

    feat: add environment and error modules

---
+++ assets/js/env.js
@@ -0,0 +1,10 @@
+export const ENV = (() => {
+  const host = location.hostname.toLowerCase();
+  if (host === 'localhost' || host === '127.0.0.1') return 'dev';
+  if (/test|qa|staging/.test(host)) return 'test';
+  return 'prod';
+})();
+
+export const isDev = ENV === 'dev';
+export const isTest = ENV === 'test';
+export const isProd = ENV === 'prod';
